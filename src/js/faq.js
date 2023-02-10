15f96d48723fe25963be299236fc1f95ec1a26ac
Marcus Silva <devsilvamarcus@gmail.com>
2023-02-10 19:19:23 -0300

    pagina seguros

---
+++ src/js/faq.js
@@ -0,0 +1,13 @@
+const perguntas = document.querySelectorAll('.pergunta');
+
+perguntas.forEach(pergunta => {
+  pergunta.addEventListener('click', function () {
+    this.classList.toggle('active');
+    const conteudo = this.nextElementSibling;
+    if (conteudo.style.maxHeight) {
+      conteudo.style.maxHeight = null;
+    } else {
+      conteudo.style.maxHeight = conteudo.scrollHeight + "px";
+    }
+  });
+});
