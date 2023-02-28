be8b0d76429260b7a71d2538224cb163365bb87d
Marcus Silva <devsilvamarcus@gmail.com>
2023-02-28 15:34:18 -0300

    correcoes no javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,35 @@
-// Ativar links do Menu
-const links = document.querySelectorAll('.header-menu a')
+function ativarLinksMenu() {
+  // Ativar links do Menu
+  const links = document.querySelectorAll('.header-menu a')
 
-function ativarLink(link) {
-  const url = location.href;
-  const href = link.href;
+  function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
 
-  if(url.includes(href)) {
-    link.classList.add('ativo');
+    if (url.includes(href)) {
+      link.classList.add('ativo');
+    }
+  }
+
+  links.forEach(ativarLink)
+}
+ativarLinksMenu();
+
+function iniciarAccordion() {
+  const perguntas = document.querySelectorAll('.pergunta');
+  if (perguntas.length) {
+    //Fazer com que o 1° accordion esteja iniciado ao carregar a página.
+    perguntas[0].classList.add('ativo');
+    perguntas[0].nextElementSibling.classList.add('ativo');
+
+    function ativarAccordion() {
+      this.classList.toggle('ativo');
+      this.nextElementSibling.classList.toggle('ativo');
+    }
+
+    perguntas.forEach((pergunta) => {
+      pergunta.addEventListener('click', ativarAccordion);
+    });
   }
 }
-
-links.forEach(ativarLink)
+iniciarAccordion();
